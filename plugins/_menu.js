dd5eb3af751efb100425db959fa7b05fbfa6cd7f
GOD ZENITSU <164893015+godzenitsu@users.noreply.github.com>
2024-05-22 07:07:56 +0530

    Update _menu.js

--- plugins/_menu.js
+++ plugins/_menu.js
@@ -16,7 +16,7 @@ bot.addCommand(
     })
     const date = new Date()
     let CMD_HELP = `╭────────────────╮
-						ʟᴇᴠᴀɴᴛᴇʀ
+						𝙽𝙴𝚉𝚄𝙺𝙾-𝙼𝙳
 ╰────────────────╯
 
 ╭────────────────
@@ -86,25 +86,25 @@ bot.addCommand(
       }
     })
     const date = new Date()
-    let msg = `\`\`\`╭═══ LEVANTER ═══⊷
-┃❃╭──────────────
-┃❃│ Prefix : ${PREFIX}
-┃❃│ User : ${message.pushName}
-┃❃│ Time : ${date.toLocaleTimeString()}
-┃❃│ Day : ${date.toLocaleString('en', { weekday: 'long' })}
-┃❃│ Date : ${date.toLocaleDateString('hi')}
-┃❃│ Version : ${VERSION}
-┃❃│ Plugins : ${PLUGINS.count}
-┃❃│ Ram : ${getRam()}
-┃❃│ Uptime : ${getUptime('t')}
-┃❃╰───────────────
+    let msg = `\`\`\`╭═══ 𝙽𝙴𝚉𝚄𝙺𝙾 𝙼𝙳 ═══⊷
+┃✦╭──────────────
+┃✦│ Prefix : ${PREFIX}
+┃✦│ User : ${message.pushName}
+┃✦│ Time : ${date.toLocaleTimeString()}
+┃✦│ Day : ${date.toLocaleString('en', { weekday: 'long' })}
+┃✦│ Date : ${date.toLocaleDateString('hi')}
+┃✦│ Version : ${VERSION}
+┃✦│ Plugins : ${PLUGINS.count}
+┃✦│ Ram : ${getRam()}
+┃✦│ Uptime : ${getUptime('t')}
+┃✦╰───────────────
 ╰═════════════════⊷\`\`\`\n`
 
     if (match && commands[match]) {
-      msg += ` ╭─❏ ${textToStylist(match.toLowerCase(), 'smallcaps')} ❏\n`
+      msg += ` ╭─ ♡  ${textToStylist(match.toLowerCase(), 'smallcaps')} ❏\n`
       for (const plugin of commands[match])
         msg += ` │ ${textToStylist(plugin.toUpperCase(), 'mono')}\n`
-      msg += ` ╰─────────────────`
+      msg += ` ╰───────────────── ♡ `
 
       return await message.send(msg)
     }
